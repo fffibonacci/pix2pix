782334d62bab08656c2fbc490079118d357870ae
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-05 01:23:34 -0600

    eval_self

--- eval_Self.sh
+++ eval_Self.sh
@@ -2,4 +2,4 @@ python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
 --result_dir ~/results/newly4/cityscapes_pix2pix_Self_400epochBtoA/test_latest/images \
---output_dir ./outputs/cityscapes_pix2pix_Self_400epochBtoA
+--output_dir ~/fixed_outputs/cityscapes_pix2pix_Self_400epochBtoA
