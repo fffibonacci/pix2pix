36a5cf6c3461fbaa758de72d4306e54ba7ea2c31
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-05 01:06:53 -0600

    vicky

---
+++ eval_Self.sh
@@ -0,0 +1,6 @@
+python ./scripts/eval_cityscapes/evaluate.py \
+--save_output_images 1 \
+--cityscapes_dir ~/Github/gtFine_val \
+--result_dir ~/results/newly4/cityscapes_pix2pix_Self_400epochAtoB/test_latest/images \
+--output_dir ~/outputs/cityscapes_pix2pix_Self_400epochAtoB
+
