19aab0b4c5501e418dfe9689df870a7b1fe60f22
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-08 18:34:36 -0600

    haha

--- eval_SelfBtoA.sh
+++ eval_SelfBtoA.sh
@@ -1,5 +1,5 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_pix2pix_Self_400epoch_v2BtoA/test_latest/images \
---output_dir ~/outputs/cityscapes_pix2pix_Self_400epoch_v2BtoA_v2
+--result_dir ~/results/no_crop/cityscapes_pix2pix_Self_400epoch_v2/test_latest/images \
+--output_dir ~/outputs/no_crop/cityscapes_pix2pix_Self_400epoch_v2BtoA
---
+++ eval_master.sh
@@ -0,0 +1,5 @@
+python ./scripts/eval_cityscapes/evaluate.py \
+--save_output_images 1 \
+--cityscapes_dir ~/Github/gtFine_val \
+--result_dir ~/results/no_crop/cityscapes_pix2pix_master_allLreluB2A_v4/test_latest/images \
+--output_dir ~/outputs/no_crop/cityscapes_pix2pix_master_allLreluB2A_v4
