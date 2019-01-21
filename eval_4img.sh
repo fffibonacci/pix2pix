ca3ea70ba9a3d0ded555cb7d1e7548e9ce90edea
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-21 01:43:30 -0600

    csv

--- eval_4img.sh
+++ eval_4img.sh
@@ -1,5 +1,5 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 0 \
---cityscapes_dir ~/Github/gtFine_4img \
+--cityscapes_dir ~/Github/gtFine_val \
 --result_dir ~/results_var/BtoA/cityscapes_pix2pix_Self_400epoch_v2/realAfakeB_latest/images \
 --output_dir ~/output_var/cityscapes_pix2pix_SelfBtoA_fakeB
--- scripts/eval_cityscapes/evaluate.py
+++ scripts/eval_cityscapes/evaluate.py
@@ -34,10 +34,10 @@ def main():
                     args.caffemodel_dir + 'fcn-8s-cityscapes.caffemodel',
                     caffe.TEST)
 
-    hist_perframe = np.zeros((4, n_cl, n_cl))
+    hist_perframe = np.zeros((500, n_cl, n_cl))
 
-    with open(args.output_dir + '/evaluation_results.txt', 'w') as f:
-        f.write('Image_Name\nMean pixel accuracy\tMean class accuracy\tMean class IoU\n')
+    with open(args.output_dir + '/evaluation_results.csv', 'w') as f:
+        f.write('Image_Name, Mean pixel accuracy,Mean class accuracy,Mean class IoU\n')
        # f.write('Mean class accuracy: %f\n' % mean_class_acc)
        # f.write('Mean class IoU: %f\n' % mean_class_iou)
 
@@ -65,7 +65,7 @@ def main():
             scipy.misc.imsave(output_image_dir + '/' + idx + '_input.jpg', im) #photo
 
         mean_pixel_acc, mean_class_acc, mean_class_iou, per_class_acc, per_class_iou = get_scores(hist_perframe[i])
-        with open(args.output_dir + '/evaluation_results.txt', 'a') as f:
+        with open(args.output_dir + '/evaluation_results.csv', 'a') as f:
             f.write('%s, %f, %f, %f\n' % (idx,mean_pixel_acc,mean_class_acc,mean_class_iou))
 
 
