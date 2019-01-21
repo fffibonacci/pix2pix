958971859f39453de7eb81647e449f710329c993
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-21 00:04:18 -0600

    4imgs

--- eval_4img.sh
+++ eval_4img.sh
@@ -1,5 +1,5 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 0 \
---cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/test4img/cityscapes_pix2pix_masterBtoA/test_latest/images \
---output_dir ~/outputs4imgs/cityscapes_pix2pix_masterBtoA
+--cityscapes_dir ~/Github/gtFine_4img \
+--result_dir ~/results_var/BtoA/cityscapes_pix2pix_Self_400epoch_v2/realAfakeB_latest/images \
+--output_dir ~/output_var/cityscapes_pix2pix_SelfBtoA_fakeB
