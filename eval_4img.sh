bae8c1de4d0ed9414c2deb9bb020c149def0e1f5
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-21 00:51:01 -0600

    var4img

--- eval_4img.sh
+++ eval_4img.sh
@@ -1,5 +1,5 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 0 \
 --cityscapes_dir ~/Github/gtFine_4img \
---result_dir ~/results_var/AtoB/cityscapes_pix2pix_masterAtoB/fakeArealB_latest/images \
---output_dir ~/output_var/cityscapes_pix2pix_masterAtoB_fakeA
+--result_dir ~/results_var/AtoB/cityscapes_pix2pix_Self_400epoch_v2/fakeArealB_latest/images \
+--output_dir ~/output_var/cityscapes_pix2pix_SelfAtoB_fakeA
