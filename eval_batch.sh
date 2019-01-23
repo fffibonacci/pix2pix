4c82e7cb91477374ff83d5bc4e968e3b68fbf437
fffibonacci <39789850+fffibonacci@users.noreply.github.com>
2019-01-23 02:05:30 -0600

    Delete eval_batch.sh

--- eval_batch.sh
+++
@@ -1,5 +0,0 @@
-python ./scripts/eval_cityscapes/evaluate.py --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_unet256_pix2pix_batchAtoB/test_latest/images \
---output_dir ~/outputs/cityscapes_unet256_batchA2B \
---save_output_images 1
-~                                                 
