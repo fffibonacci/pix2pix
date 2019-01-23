354f66e9d20afc56f1241bf958552c46cef93ec9
fffibonacci <39789850+fffibonacci@users.noreply.github.com>
2019-01-23 02:03:28 -0600

    Delete eval_ngfBtoA.sh

--- eval_ngfBtoA.sh
+++
@@ -1,5 +0,0 @@
-python ./scripts/eval_cityscapes/evaluate.py \
---save_output_images 1 \
---cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/right_results/BtoA/cityscapes_pix2pix_Self_ngf96_400epoch/test_latest/images \
---output_dir ~/right_outputs/cityscapes_pix2pix_Self_ngf96_400epochBtoA_2
