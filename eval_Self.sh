1f2e22f230597650ce3767ce7f4272c932584d51
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-31 09:37:02 -0600

    back

--- eval_Self.sh
+++ eval_Self.sh
@@ -1,6 +1,6 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 1 \
---cityscapes_dir ~/Github/gtFine_vis \
---result_dir ~/vis/masterAtoB/ \
---output_dir ~/vis_outputs/masterAtoB
+--cityscapes_dir ~/Github/gtFine_val \
+--result_dir ~/results/BtoA/cityscapes_pix2pix_Self_400epoch_v2/test_latest/images \
+--output_dir ~/outputs/cityscapes_pix2pix_Self_400epochBtoA_v2
 
--- scripts/eval_cityscapes/evaluate.py
+++ scripts/eval_cityscapes/evaluate.py
@@ -7,7 +7,6 @@ import scipy.misc
 from PIL import Image
 from util import *
 from cityscapes import cityscapes
-labels = __import__('labels')
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--cityscapes_dir", type=str, required=True, help="Path to the original cityscapes dataset")
@@ -29,16 +28,14 @@ def main():
     CS = cityscapes(args.cityscapes_dir)
     n_cl = len(CS.classes)
     label_frames = CS.list_label_frames(args.split)
-#    caffe.set_device(args.gpu_id)
-#    caffe.set_mode_gpu()
-#    net = caffe.Net(args.caffemodel_dir + 'deploy.prototxt',
-#                    args.caffemodel_dir + 'fcn-8s-cityscapes.caffemodel',
-#                    caffe.TEST)
+    caffe.set_device(args.gpu_id)
+    caffe.set_mode_gpu()
+    net = caffe.Net(args.caffemodel_dir + '/deploy.prototxt',
+                    args.caffemodel_dir + 'fcn-8s-cityscapes.caffemodel',
+                    caffe.TEST)
 
     hist_perframe = np.zeros((n_cl, n_cl))
     for i, idx in enumerate(label_frames):
-       # if( i>10 ):
-       #     break
         if i % 10 == 0:
             print('Evaluating: %d/%d' % (i, len(label_frames)))
         city = idx.split('_')[0]
@@ -46,35 +43,17 @@ def main():
         label = CS.load_label(args.split, city, idx)
         im_file = args.result_dir + '/' + idx + '_fake_B.png' 
         im = np.array(Image.open(im_file))
-#        im = im[:,:,0:3]
-#        print(im.dtype, label.dtype)
-        im = scipy.misc.imresize(im, (256, 256),interp='nearest')
-        im_label = np.zeros((256,256))
-        # change prediction image from color to label using neighbor 
-        for a in range(256):
-            for j in range(256):
-                color = im[a][j]
-                im_label[a][j] = neighbor_id(color)
-        #im_label = im_label[np.newaxis, ...]       
-        #out = segrun(net, CS.preprocess(im_label))
-        # print(im_label.flatten().shape) 
-        im_label = scipy.misc.imresize(im_label,(1024,2048),interp='nearest')
-        #np.int8!!!!!overflow
-	label = np.squeeze(label)
-        im_label = np.array(im_label,dtype = np.uint8)
-	#index = np.where((im_label.flatten()==13) & (label.flatten()==13))
-	#print(index)
-        false_im = (im_label==label)*255
-        hist_perframe += fast_hist(label.flatten(), im_label.flatten(), n_cl)
-	im = scipy.misc.imresize(im,(1024,2048))
+        # im = scipy.misc.imresize(im, (256, 256))
+        im = scipy.misc.imresize(im, (label.shape[1], label.shape[2]))
+        out = segrun(net, CS.preprocess(im))
+        hist_perframe += fast_hist(label.flatten(), out.flatten(), n_cl)
         if args.save_output_images > 0:
             label_im = CS.palette(label)
-            pred_im = CS.palette(im_label)
-            scipy.misc.imsave(output_image_dir + '/' + idx + '_resize_color.jpg', im)
-            scipy.misc.imsave(output_image_dir + '/' + idx + '_gt.jpg', label_im)
-            scipy.misc.imsave(output_image_dir + '/' + idx + '_input_trainId.jpg', im_label)
-            scipy.misc.imsave(output_image_dir + '/' + idx + '_3gt.jpg', im)
-            scipy.misc.imsave(output_image_dir + '/' + idx + '_false.jpg', false_im)
+            pred_im = CS.palette(out)
+            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_pred.jpg', pred_im)
+            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_gt.jpg', label_im)
+            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_input.jpg', im)
+
     mean_pixel_acc, mean_class_acc, mean_class_iou, per_class_acc, per_class_iou = get_scores(hist_perframe)
     with open(args.output_dir + '/evaluation_results.txt', 'w') as f:
         f.write('Mean pixel accuracy: %f\n' % mean_pixel_acc)
@@ -85,17 +64,4 @@ def main():
             while len(cl) < 15:
                 cl = cl + ' '
             f.write('%s: acc = %f, iou = %f\n' % (cl, per_class_acc[i], per_class_iou[i]))
-def neighbor_id(color):
-    min_dist = 1000
-    min_id = -1
-    for i in range(len(labels.labels)):
-        a = labels.labels[i].color - color
-        dist = abs(a[0]) + abs(a[1]) + abs(a[2])
-        #min_dist = min(min_dist, dist)
-        if(min_dist > dist):
-             min_dist = dist
-             min_id = labels.labels[i].trainId
-  	
-    return min_id
-
 main()
