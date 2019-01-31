3a1bdd838b1fd51143a2217443809df4d265f23c
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-01-31 11:56:45 -0600

    scripts

--- eval_Self.sh
+++ eval_Self.sh
@@ -1,6 +1,5 @@
 python ./scripts/eval_cityscapes/evaluate.py \
 --save_output_images 1 \
---cityscapes_dir ~/Github/gtFine_vis \
---result_dir ~/vis/masterBtoA \
---output_dir ~/vis_outputs/
---
+--cityscapes_dir ~/Github/gtFine_val \
+--result_dir ~/results_var/AtoB/cityscapes_pix2pix_Self_400epoch_v2/fakeArealB_latest/images \
+--output_dir ~/outputs_var/cityscapes_pix2pix_SelfAtoB_fakeA
