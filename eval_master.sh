832ccab8cb82dcbbdadce204ac060d3eac897187
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-03-02 22:23:11 -0600

    master'

--- eval_master.sh
+++ eval_master.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results_init/BtoA/cityscapes_pix2pix_AtoB_self_v2/test_latest/images \
---output_dir ~/outputs/cityscapes_pix2pix_BtoA_self_v2
+--result_dir ~/results_init_selfv2/BtoA/cityscapes_pix2pix_AtoB_self_v3/test_latest/images \
+--output_dir ~/outputs/cityscapes_pix2pix_BtoA_self_v3
