43d29b2f57cc9dc159799503d64d1d38dce46478
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-04-09 02:44:02 -0500

    AB

--- eval_AB_v2.sh
+++ eval_AB_v2.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_photo2label_pretrained_v2/fakeA \
+--result_dir ~/results/cityscapes_photo2label_pretrained_v2/fakeB \
 --output_dir ~/outputs/cityscapes_AtoB_pretrained_v2
