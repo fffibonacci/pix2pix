5fdf8b417d65c7368bbac78c5acf2b14f35bfe71
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-04-09 02:38:55 -0500

    BA

--- eval_master.sh
+++ eval_master.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_photo2label_pretrained_v2/fakeB \
+--result_dir ~/results/cityscapes_photo2label_pretrained_v2/fakeA \
 --output_dir ~/outputs/cityscapes_BtoA_pretrained_v2
