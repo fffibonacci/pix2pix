975ece5ae226fe9bdecb25a5b7012645c2648b1a
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-04-09 02:05:20 -0500

    add v2

--- eval_AB.sh
+++ eval_AB.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/test_latest/images \
+--result_dir ~/results/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/test_120/fakeA \
 --output_dir ~/outputs/cityscapes_Self_cycle_2Dis_noRec_SepLoss_AtoB
