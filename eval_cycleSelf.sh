4f5047f580dc9f08fd9d750b37ac2937ad9dc887
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-04-09 02:11:05 -0500

    selfcycle

---
+++ eval_cycleSelf.sh
@@ -0,0 +1,4 @@
+python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
+--cityscapes_dir ~/Github/gtFine_val \
+--result_dir ~/results/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/test_120/fakeB \
+--output_dir ~/outputs/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss_test120
