d9956e9769f81a174d41474c3f8c2dbdda65bf18
fffibonacci <yifanchen@intl.zju.edu.cn>
2019-04-09 00:20:13 -0500

    evalpretrained

--- eval_AB.sh
+++ eval_AB.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_photo2label_pretrained_1024/test_latest/images \
---output_dir ~/outputs/cityscapes_AtoB_pretrained_1024
+--result_dir ~/results/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss/test_latest/images_photo \
+--output_dir ~/outputs/cityscapes_Self_cycle_2Dis_noRec_SepLoss_BtoA
--- eval_master.sh
+++ eval_master.sh
@@ -1,4 +1,4 @@
 python ./scripts/eval_cityscapes/evaluate.py --save_output_images 1 \
 --cityscapes_dir ~/Github/gtFine_val \
---result_dir ~/results/cityscapes_photo2label_pretrained_1024/test_latest/images_fakeB \
---output_dir ~/outputs/cityscapes_BtoA_pretrained_1024
+--result_dir ~/results/cityscapes_photo2label_pretrained_v2/fakeB \
+--output_dir ~/outputs/cityscapes_BtoA_pretrained_v2
